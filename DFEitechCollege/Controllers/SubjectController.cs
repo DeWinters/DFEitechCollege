2cfe29a92e60da42351f7bd871c5a919a0cc13ca
DeWinters <derekwwinters@gmail.com>
2017-02-15 23:42:30 +0000

    Subject Model

---
+++ DFEitechCollege/Controllers/SubjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DFEitechCollege.Controllers
+{
+    public class SubjectController : Controller
+    {
+        // GET: Subject
+        public ActionResult OpenSubjects()
+        {
+            return View();
+        }
+    }
+}
---
+++ DFEitechCollege/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DFEitechCollege.Models
+{
+    public class Subject
+    {
+        public int subjectId { get; set; }
+        public string subjectName { get; set; }
+        public Boolean subjectHigher { get; set; }
+    }
+}
