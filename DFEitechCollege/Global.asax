2421be5fdf3a3f836a254b7d7e768ee8dffaff2f
DeWinters <derekwwinters@gmail.com>
2017-02-15 22:54:34 +0000

    Add project files.

---
+++ DFEitechCollege/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DFEitechCollege.MvcApplication" Language="C#" %>
---
+++ DFEitechCollege/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DFEitechCollege.Startup))]
+namespace DFEitechCollege
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
